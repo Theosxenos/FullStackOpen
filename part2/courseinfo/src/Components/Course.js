a2774fed2cce8f9ff4c262030a87c26b77a16245
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2023-07-22 11:48:03 +0200

    Split into component files

---
+++ part2/courseinfo/src/Components/Course.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import Content from "./Content";
+
+const Course = (props) => {
+    console.log(props);
+    return <>
+        <Header />
+    </>
+}
+
+export default Course;
---
+++ part2/courseinfo/src/Components/Total.js
@@ -0,0 +1,3 @@
+const Total = (props) => (
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+)
