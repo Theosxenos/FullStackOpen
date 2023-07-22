48aa141146afa552677bb34aa1a8d27717f67634
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2023-07-22 14:27:34 +0200

    Finish 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,34 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas'}
+    ])
+    const [newName, setNewName] = useState('')
 
-  return (
-      <div>
-        <h2>Phonebook</h2>
-        <form>
-          <div>
-            name: <input />
-          </div>
-          <div>
-            <button type="submit">add</button>
-          </div>
-        </form>
-        <h2>Numbers</h2>
-        ...
-      </div>
-  )
+    const addPerson = (event) => {
+      event.preventDefault();
+      
+      const newPerson = {name: newName};
+      setPersons([...persons, newPerson]);
+      setNewName('');
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map((person) => <div key={person.name}>{person.name}</div>)}
+        </div>
+    )
 }
 
 export default App
