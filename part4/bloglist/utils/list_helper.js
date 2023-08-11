44cc5309eef4a1a16ecfa63ff2d52af0f41eb46c
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2023-08-11 12:21:26 +0200

    Finish 4.4 up to including 4.7

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,4 +1,37 @@
+// eslint-disable-next-line import/no-extraneous-dependencies,import/extensions
+import _ from 'lodash';
+
+// import groupBy from 'lodash/groupBy';
+// import map from 'lodash/map';
+// import maxBy from 'lodash/maxBy';
+
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1;
 
-export default dummy;
+const totalLikes = (blogs) => blogs
+    .reduce((currentSum, currentBlog) => currentSum + currentBlog.likes, 0);
+
+const favoriteBlog = (blogs) => blogs
+    .reduce((prev, current) => ((prev.likes > current.likes) ? prev : current));
+
+const mostBlogs = (blogs) => _.chain(blogs)
+    .groupBy('author')
+    .map((authorBlogs, author) => ({
+        author,
+        blogs: authorBlogs.length,
+    }))
+    .maxBy('blogs')
+    .value();
+
+const mostLikes = (blogs) => _.chain(blogs)
+    .groupBy('author')
+    .map((authorBlogs, author) => ({
+        author,
+        likes: _.sumBy(authorBlogs, 'likes'),
+    }))
+    .maxBy('likes')
+    .value();
+
+export {
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes,
+};
