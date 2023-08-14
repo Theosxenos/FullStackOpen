0613fda82ce6770d74348d93b6006fec3d540315
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2023-08-14 13:33:53 +0200

    Update .eslintrc.cjs

--- part4/bloglist/.eslintrc.cjs
+++ part4/bloglist/.eslintrc.cjs
@@ -8,6 +8,13 @@ module.exports = {
   extends: 'airbnb-base',
   rules: {
     indent: ['error', 4], // Set 4 spaces for indentation
+    'import/extensions': ['warn', 'ignorePackages', {
+      'js': 'never',
+      'mjs': 'never',
+      'jsx': 'never',
+      'ts': 'never',
+      'tsx': 'never'
+    }],
   },
   parserOptions: {
     ecmaVersion: 'latest',
