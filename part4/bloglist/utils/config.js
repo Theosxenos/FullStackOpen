fe3f5221f3f251292fff9a7a8f259e3bbd448f04
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2023-08-21 10:04:40 +0200

    Update config.js

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,6 +1,11 @@
 import 'dotenv/config';
 
-const { NODE_ENV, MONGODB_DB_TEST, MONGODB_DB_PROD, MONGO_CONNECTION } = process.env;
+const {
+    NODE_ENV,
+    MONGODB_DB_TEST,
+    MONGODB_DB_PROD,
+    MONGO_CONNECTION,
+} = process.env;
 
 const MONGODB_DB = NODE_ENV === 'test' ? MONGODB_DB_TEST : MONGODB_DB_PROD;
 
